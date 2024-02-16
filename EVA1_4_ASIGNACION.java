ab1febe06aed298ad09eaff5e775bbc556bd0f09
Arena <159494917+ArenaPerezFranco@users.noreply.github.com>
2024-02-15 21:13:02 -0700

    Add files via upload

---
+++ EVA1_4_ASIGNACION.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_ASIGNACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;//DECLARACIÓN
+        edad = 18;//ASIGNACIÓN--> UN SOLO = SIGNIFICA ASIGNAR
+        System.out.println("Tu edad es:");
+        System.out.println(edad);
+        
+        
+        String nombre;//Declaración
+        nombre = "Leah Arena Perez Franco" ;
+        System.out.println("Tu nombre es:");
+        System.out.println(nombre);
+        
+        
+        String carrera;
+        carrera = "Ing.SistemasComputacionales";
+        System.out.println ("Estas en la carrera:");
+        System.out.println(carrera);
+    }
+    
+}
